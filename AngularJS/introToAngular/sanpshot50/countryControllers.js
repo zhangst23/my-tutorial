6ec4ebc810859455590b2a41114114860577d53e
栋栋晓 <2243980491@qq.com>
2015-11-08 16:12:01 +0800

    demo

---
+++ AngularJS/introToAngular/sanpshot50/countryControllers.js
@@ -0,0 +1,14 @@
+
+var countryControllers = angular.module('countryControllers',[]);
+
+countryControllers.controller('CountryListCtrl',function($scope,countries){
+	countries.list(function(countries){
+		$scope.countries = countries;
+	});
+});
+
+countryControllers.controller('CountryDetailCtrl',function($scope,$routeParams,countries){
+	countries.find($routeParams.countryId,function(country){
+		$scope.country = country;
+	})
+})
