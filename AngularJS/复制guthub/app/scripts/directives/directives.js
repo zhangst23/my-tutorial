d14982e6e84ac33d25a6f2ad2d29710b03320c18
栋栋晓 <2243980491@qq.com>
2015-11-17 04:06:21 +0800

    u

---
+++ AngularJS/复制guthub/app/scripts/directives/directives.js
@@ -0,0 +1,25 @@
+var directives = angular.module('guthub.directives',[]);
+
+directives.directive('butterbar',['$rootScope',function($rootScope){
+	return{
+		link:function(scope,element,attrs){
+			element.addClass('hide');
+
+			$rootScope.$on('$routeChangeStart',function(){
+				element.removeClass('hide');
+			})
+
+			$rootScope.$on('$routeChangeSuccess',function(){
+				element.addClass('hide');
+			});
+
+		}
+	}
+}])
+directives.directive('focus',function(){
+				return{
+					function(scope,element,attrs){
+						element[0].focus();
+					}
+				}
+});
