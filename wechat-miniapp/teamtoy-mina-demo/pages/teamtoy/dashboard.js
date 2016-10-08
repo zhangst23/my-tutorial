f249a518379e851b72883042c03ec3dbec025a08
栋栋晓 <2243980491@qq.com>
2016-10-09 00:49:49 +0800

    u

---
+++ wechat-miniapp/teamtoy-mina-demo/pages/teamtoy/dashboard.js
@@ -0,0 +1,46 @@
+var app = getApp()
+Page({
+	data:{
+		todolist:[]
+	},
+	todoAdd:function(e)
+	{
+		//console.log(e);
+		wx.navigateTo({'url':'todoadd'});
+	},
+	onLoad: function()
+	{
+		var that = this;
+		var url = 'http://'+app.globalData.ttHost+'/?c=api&a=todo_list&token='+app.globalData.ttToken;
+		wx.request({
+			'url':url,
+			header: {
+				'Content-Type':'application/json'
+			},
+			success:function(res)
+			{
+				// console.log(res)
+				if (res.data && res.data.data) {
+					that.setData({todolist:res.data.data});
+				}
+				console.log( that.data );
+			}
+		});
+	},
+	onShow:function(){
+		this.onLoad();
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
