61b77a0625590214db82b8b85ee28bee6159af59
栋栋晓 <2243980491@qq.com>
2016-10-10 00:44:42 +0800

    u

--- wechat-miniapp/FanQie-timer/app.js
+++ wechat-miniapp/FanQie-timer/app.js
@@ -1,30 +1,24 @@
 //app.js
+const defaultTime = {
+  defaultWorkTime: 25,
+  defaultRestTime: 5
+}
+
 App({
   onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
+    let workTime = wx.getStorageSync('workTime')
+    let restTime = wx.getStorageSync('restTime')
+    if (!workTime) {
+      wx.setStorage({
+        key: 'workTime',
+        data: defaultTime.defaultWorkTime
+      })
+    }
+    if (!restTime) {
+      wx.setStorage({
+        key: 'restTime',
+        data: defaultTime.defaultRestTime
       })
     }
   },
-  globalData:{
-    userInfo:null
-  }
 })
